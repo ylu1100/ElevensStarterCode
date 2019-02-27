122359dc5c24b740a59c3c1b04602b870e0bb89c
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-02-27 08:43:59 -0500

    Update Deck.java

--- src/Activity2/Deck.java
+++ src/Activity2/Deck.java
@@ -48,6 +48,7 @@ public class Deck {
                 cards.add(new Card(ranks[i],suits[x],values[i]));
             }
         }
+        shuffle();
         size=cards.size();
     }
 
@@ -91,8 +92,8 @@ public class Deck {
             cards.remove(chosencard);/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
         }
         for(int i = 0;i<shuffledeck.size();i++){
-            cards.add(shuffledeck.get(i));
-            shuffledeck.remove(i);
+            cards.add(shuffledeck.get(0));
+            shuffledeck.remove(0);
         }
         size=cards.size();
     }
